5834fda86b6e7cb5c3059e5fd3a781c67c1fac11
James Rasekoala <jamesrasekoala@gmail.com>
2019-02-18 14:19:58 +0200

    Setting up repo

---
+++ MyC++/DatabaseDriver/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    /*
+    system("Student Recods database");
+    sytem("cis");*/
+    cout << "Hello world!" << endl;
+    return 0;
+}
